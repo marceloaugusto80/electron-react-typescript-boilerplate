cd6ded0e28e1eb5b106c71bd8ff90b6dfc485e3a
Marcelo Augusto <marceloaugustoxp@gmail.com>
2022-06-26 00:13:42 +0100

    use createRoot

--- src/renderer/renderer-process.tsx
+++ src/renderer/renderer-process.tsx
@@ -1,5 +1,10 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import React from "react";
 import MainLayout from "./MainLayout";
 
-render(<MainLayout />, document.getElementById("root"));
+const container = document.getElementById("root");
+if(!container) throw Error("Root element not found.");
+
+const root = createRoot(container);
+
+root.render(<MainLayout />);
